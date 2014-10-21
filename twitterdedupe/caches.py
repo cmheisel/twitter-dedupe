9a6a143d079f1790b8205fa2f864c2f258be4d2a
Chris Heisel <chris@heisel.org>
2014-10-21 14:20:37 -0400

    Added RedisCache and redis library requirement

--- twitterdedupe/caches.py
+++ twitterdedupe/caches.py
@@ -7,3 +7,16 @@ class LocalCache(object):
 
     def get(self, key, default=None):
         return self.data.get(key, default)
+
+
+class RedisCache(object):
+    def __init__(self, redis):
+        self.redis = redis
+
+    def set(self, key, value, timeout=30):
+        self.redis.setex(key, timeout, value)
+
+    def get(self, key, default=None):
+        result = self.redis.get(key)
+        if result is None:
+            return default
