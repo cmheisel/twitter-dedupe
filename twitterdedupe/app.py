8197b7b9946506455e9f64cc4fc9fc0b4d383a03
Chris Heisel <chris@heisel.org>
2014-10-21 16:52:00 -0400

    Added url lengthening and plain text retweeing

--- twitterdedupe/app.py
+++ twitterdedupe/app.py
@@ -1,5 +1,6 @@
 import tweepy
 import logging
+import requests
 
 
 def login(consumer_key, consumer_secret, access_token, access_token_secret):
@@ -21,6 +22,14 @@ def _key(screen_name, url):
     return "%s_%s" % (screen_name, url)
 
 
+def lengthen_url(url):
+    try:
+        r = requests.get(url)
+        return r.url
+    except Exception:
+        return url
+
+
 def get_unique_statuses(api, screen_name, since_id, cache, cache_length=604800):
     logger = logging.getLogger("twitterdedupe.get_unique_statuses")
     logger.debug("%s -- since %s" % (screen_name, since_id))
@@ -33,13 +42,18 @@ def get_unique_statuses(api, screen_name, since_id, cache, cache_length=604800):
         timeline = api.user_timeline(screen_name=screen_name, since_id=since_id, page=page)
         for s in timeline:
             for url in s.entities['urls']:
-                expanded_url = url['expanded_url']
+                expanded_url = lengthen_url(url['expanded_url'])
                 key = _key(screen_name, expanded_url)
                 url_count = cache.get(key, None)
-                logger.info("%s - %s -- %s" % (s.id, expanded_url, url_count))
                 if url_count is None:
                     stati.append(s)
                     cache.set(key, 1, cache_length)
+                    logger.info("%s - %s" % (s.id, expanded_url))
+            if len(s.entities['urls']) == 0:
+                # Let text only tweets pass through
+                # TODO: Add MD5 checking because maybe someone will make a noisy
+                # text account
+                stati.append(s)
         if len(timeline) > 0:
             page += 1
         else:
