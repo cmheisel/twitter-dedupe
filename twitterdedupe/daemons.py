a3d03b317784e53f590daf13bde8f602382315ad
Chris Heisel <chris@heisel.org>
2014-10-23 12:14:47 -0400

    Fix spelling, inning issues with log daemon

--- twitterdedupe/daemons.py
+++ twitterdedupe/daemons.py
@@ -9,6 +9,14 @@ from twitterdedupe import login, get_my_unique_statuses, get_since_id
 from twitterdedupe.caches import RedisCache
 
 
+class Whitelist(logging.Filter):
+    def __init__(self, *whitelist):
+        self.whitelist = [logging.Filter(name) for name in whitelist]
+
+    def filter(self, record):
+        return any(f.filter(record) for f in self.whitelist)
+
+
 class LoggingOnlyDaemon(object):
     """
     Daemon that will only log unique tweets, DOES NOT RETWEET
@@ -17,20 +25,20 @@ class LoggingOnlyDaemon(object):
 
     def __init__(self, env):
         self.consumer_key = env['TWITTER_CONSUMER_KEY']
-        self.consumer_secret = env['TWITTER_ONSUMER_SECRET']
+        self.consumer_secret = env['TWITTER_CONSUMER_SECRET']
         self.access_token = env['TWITTER_ACCESS_TOKEN']
         self.access_token_secret = env['TWITTER_ACCESS_TOKEN_SECRET']
         self.redis_url = env['REDISTOGO_URL']
         self.screen_name = env['TWITTER_SCREEN_NAME']
-        self.interval = env['WAIT_INTERVAL']
+        self.interval = int(env['WAIT_INTERVAL'])
         self.log_level = getattr(logging, env['LOG_LEVEL'])
 
         self.redis = redis.from_url(self.redis_url)
-        self.cache = RedisCache(redis, "cache|%s|" % self.screen_name)
+        self.cache = RedisCache(self.redis, "cache|%s|" % self.screen_name)
 
         logging.basicConfig(level=self.log_level)
         for handler in logging.root.handlers:
-            handler.addFilter('twitterdedupe')
+            handler.addFilter(Whitelist(self.screen_name, 'twitterdedupe'))
         self.logger = logging.getLogger(self.screen_name)
 
         self.api = login(self.consumer_key, self.consumer_secret,
