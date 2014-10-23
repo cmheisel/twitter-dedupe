6d4983354e3cadf1e3d1000744d0c261761b7528
Chris Heisel <chris@heisel.org>
2014-10-23 12:15:08 -0400

    Add setup.py and local install

---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages, Command
+
+version = "0.1"
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys
+        import subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+
+setup(
+    name="twitter-dedupe",
+    version=version,
+    author="Chris Heisel",
+    author_email="chris@heisel.org",
+    description=("Python library to retweet unique links from noisy Twitter accounts."),
+    long_description=open("README.md").read(),
+    url="https://github.com/cmheisel/twitter-dedupe",
+    zip_safe=False,
+    include_package_data=True,
+    scripts=['bin/logonly.py', ],
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst', '.html'],
+    },
+    packages=find_packages(),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    install_requires=[
+        "redis",
+        "requests",
+        "tweepy",
+    ],
+    cmdclass = {'test': PyTest},
+)
